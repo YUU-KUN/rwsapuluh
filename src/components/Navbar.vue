7dc0735628adb675c5ca2a26534b73b5ad7b9883
Indra <iwahyu@student.telkomuniversity.ac.id>
2023-07-11 09:13:31 +0700

    [Fix] Fixed link routing in footernavbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,9 @@
         data-te-navbar-ref>
         <div class="flex w-full flex-wrap items-center justify-between px-3">
             <!-- Left elements -->
-            <IconMain />
+            <router-link to="/">
+                <IconMain />
+            </router-link>
 
             <!-- Right elements -->
             <div class="relative flex items-center">
@@ -42,7 +44,8 @@
                 id="navbarSupportedContent1" data-te-collapse-item>
                 <ul class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row text-right" data-te-navbar-nav-ref>
                     <li class="mb-4 laptop:self-center lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                        <router-link to="/" class="text-black hover:text-primary focus:text-primary lg:px-2 [&.active]:text-black/90"
+                        <router-link to="/"
+                            class="text-black hover:text-primary focus:text-primary lg:px-2 [&.active]:text-black/90"
                             data-te-nav-link-ref>Beranda</router-link>
                     </li>
                     <li class="relative mb-4 laptop:self-center lg:mb-0 lg:pr-2" data-te-nav-item-ref data-te-dropdown-ref>
@@ -54,44 +57,46 @@
                         <!-- First dropdown menu -->
                         <ul class="absolute left-auto right-0 z-[1000] float-left m-0 mt-1 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
                             aria-labelledby="dropdown-about-us" data-te-dropdown-menu-ref>
-                            <li>
+                            <li class="bg-white hover:bg-primary">
                                 <router-link to="/history"
-                                    class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    class="hover:text-white block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-black active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent"
                                     data-te-dropdown-item-ref>Sejarah</router-link>
                             </li>
-                            <li>
+                            <li class="bg-white hover:bg-primary">
                                 <router-link to="/organization-structure"
-                                    class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    class="hover:text-white block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-black active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent"
                                     data-te-dropdown-item-ref>Struktur Organisasi</router-link>
                             </li>
                         </ul>
                     </li>
                     <li class="mb-4 laptop:self-center lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                        <a class="text-black hover:text-primary focus:text-primary lg:px-2 [&.active]:text-black/90"
-                            data-te-nav-link-ref>Layanan</a>
+                        <router-link to="/service"
+                            class="text-black hover:text-primary focus:text-primary lg:px-2 [&.active]:text-black/90"
+                            data-te-nav-link-ref>Layanan</router-link>
                     </li>
                     <li class="relative mb-4 laptop:self-center lg:mb-0 lg:pr-2" data-te-nav-item-ref data-te-dropdown-ref>
                         <a @click="getInstitutions()"
                             class="hidden-arrow flex items-center justify-end text-black hover:text-primary focus:text-primary lg:px-2 [&.active]:text-black/90 "
-                            id="dropdown-institution" role="button" data-te-dropdown-toggle-ref
-                            aria-expanded="false">
+                            id="dropdown-institution" role="button" data-te-dropdown-toggle-ref aria-expanded="false">
                             Kelembagaan
                             <img src="/src/assets/icons/arrow-down.png" alt="arrow-down" class="ml-1">
                         </a>
                         <!-- First dropdown menu -->
                         <ul class="absolute left-auto right-0 z-[1000] float-left m-0 mt-1 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
                             aria-labelledby="dropdown-institution" data-te-dropdown-menu-ref>
-                            <!-- First dropdown menu items -->
-                            <li v-for="{id, name} in institutions" :key="id" class="bg-white">
-                                <router-link :to="{name: 'institution', params: {institution_id: id}}" class="hover: block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                                    data-te-dropdown-item-ref>{{ name }}</router-link>
-                            </li>
+                            <div :class="institutions.length > 5 ? 'grid grid-rows-5 grid-flow-col bg-white' : ''">
+                                <!-- First dropdown menu items -->
+                                <li v-for="{ id, name } in institutions" :key="id" class="bg-white hover:bg-primary">
+                                    <router-link :to="{ name: 'institution', params: { institution_id: id } }"
+                                        class="hover:text-white block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-black active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400"
+                                        data-te-dropdown-item-ref>{{ name }}</router-link>
+                                </li>
+                            </div>
                         </ul>
                     </li>
                     <li class="relative mb-4 laptop:self-center lg:mb-0 lg:pr-2" data-te-nav-item-ref data-te-dropdown-ref>
                         <a class="hidden-arrow flex items-center justify-end text-black hover:text-primary focus:text-primary lg:px-2 [&.active]:text-black/90 "
-                            id="dropdownMenuButton1" role="button" data-te-dropdown-toggle-ref
-                            aria-expanded="false">
+                            id="dropdownMenuButton1" role="button" data-te-dropdown-toggle-ref aria-expanded="false">
                             Informasi
                             <img src="/src/assets/icons/arrow-down.png" alt="arrow-down" class="ml-1">
                         </a>
@@ -99,14 +104,14 @@
                         <ul class="absolute left-auto right-0 z-[1000] float-left m-0 mt-1 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
                             aria-labelledby="dropdownMenuButton1" data-te-dropdown-menu-ref>
                             <!-- First dropdown menu items -->
-                            <li>
+                            <li class="bg-white hover:bg-primary">
                                 <router-link to="/achievement"
-                                    class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    class="hover:text-white block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-black active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent"
                                     data-te-dropdown-item-ref>Prestasi</router-link>
                             </li>
-                            <li>
+                            <li class="bg-white hover:bg-primary">
                                 <router-link to="/activity"
-                                    class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    class="hover:text-white block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-black active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent"
                                     data-te-dropdown-item-ref>Kegiatan</router-link>
                             </li>
                         </ul>
