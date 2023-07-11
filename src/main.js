5619871c2901c31431ee2e64eb805d4e0ffb0fff
Indra <iwahyu@student.telkomuniversity.ac.id>
2023-07-11 09:12:46 +0700

    [Fix] Changed axios baseurl for production mode

--- src/main.js
+++ src/main.js
@@ -13,6 +13,6 @@ const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
 // eslint-disable-next-line no-undef
-axios.defaults.baseURL = process.env.NODE_ENV == 'development' ? 'http://127.0.0.1:8000/api' : 'https://api.rwsapuluhcipagalo.or.id/api'
+axios.defaults.baseURL = process.env.NODE_ENV == 'development' ? 'http://127.0.0.1:8000/api' : 'https://api.rwsapuluhcipagalo.com/api'
 
 app.mount('#app')
